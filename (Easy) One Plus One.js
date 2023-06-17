190a9450b646727e48fe2211185bbc5369c8995b
Joseph Lim <limjosephjaine@gmail.com>
2023-06-17 17:52:01 +0800

    Update

---
+++ (Easy) One Plus One.js
@@ -0,0 +1,43 @@
+/*
+Create a function that outputs the result of a math expression in words.
+
+//-- FOR TESTING --//
+
+wordedMath("One plus one") ➞ "Two"
+
+wordedMath("zero Plus one") ➞ "One"
+
+wordedMath("one minus one") ➞ "Zero"
+
+NOTES:
+    Expect only the operations plus and minus.
+
+    Expect to only get numbers and answers from 0 to 2.
+
+    The first letter of the answer must be capitalised.
+
+*/
+
+const wordedMath = (arr) => {
+    let t = []
+    const num = ["zero","one","two"];
+    let ans = 0;
+
+    t = arr.split(" ").map(el => num.indexOf(el.toLowerCase()) >= 0 ? num.indexOf(el.toLowerCase()) : el)
+
+    switch(t[1].toLowerCase())
+    {
+        case "plus": 
+            ans = t[0] + t[2];
+            break;
+        case "minus":
+            ans = t[0] - t[2];
+            break;
+    }
+
+    return num[ans].replace(/^./, str => str.toUpperCase())
+}
+
+console.log(wordedMath("One plus one")) // "Two"
+console.log(wordedMath("zero Plus one")) // "One"
+console.log(wordedMath("one minus one")) // "Zero"
