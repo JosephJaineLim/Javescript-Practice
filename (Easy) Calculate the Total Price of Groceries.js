58c5e186e8fa80a5ebe6a2c8ce64557af457bd75
Joseph Lim <limjosephjaine@gmail.com>
2023-06-21 16:14:32 +0800

    Update

--- (Easy) Calculate the Total Price of Groceries.js
+++ (Easy) Calculate the Total Price of Groceries.js
@@ -37,23 +37,14 @@ getTotalPrice([
 
 */
 
-const getTotalPrice = (arr) => {
-    let t = 0;
-    
-    arr.forEach(element => {
-       t += element.price * element.quantity; 
-    });
-    
-
-    return t.toFixed(2);
-}
+const getTotalPrice = (arr) => arr.reduce((accumulator, current) => accumulator + current.price * current.quantity,0).toFixed(2)
 
 
 console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.50 }]));
 // 1.5
 console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.50 },{ product: "Cereals", quantity: 1, price: 2.50 }]));
 //4
-console.log(getTotalPrice([{ product: "Milk", quantity: 3, price: 1.50 }]) );
+console.log(getTotalPrice([{ product: "Milk", quantity: 3, price: 1.50 }]));
 //4.5
 console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.50 },{ product: "Eggs", quantity: 12, price: 0.10 },{ product: "Bread", quantity: 2, price: 1.60 },{ product: "Cheese", quantity: 1, price: 4.50 }]));
 //10.4
