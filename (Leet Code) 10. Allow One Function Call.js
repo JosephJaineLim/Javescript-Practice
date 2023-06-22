0a360492606dfc822f4858fcb6947c8cb3347c92
Joseph Lim <limjosephjaine@gmail.com>
2023-06-22 12:39:17 +0800

    Update

---
+++ (Leet Code) 10. Allow One Function Call.js
@@ -0,0 +1,45 @@
+/*
+Given a function fn, return a new function that is identical to the original function except 
+that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+ 
+
+Example 1:
+    Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+    Output: [{"calls":1,"value":6}]
+    Explanation:
+    const onceFn = once(fn);
+    onceFn(1, 2, 3); // 6
+    onceFn(2, 3, 6); // undefined, fn was not called
+
+Example 2:
+    Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+    Output: [{"calls":1,"value":140}]
+    Explanation:
+    const onceFn = once(fn);
+    onceFn(5, 7, 4); // 140
+    onceFn(2, 3, 6); // undefined, fn was not called
+    onceFn(4, 6, 8); // undefined, fn was not called
+*/
+const once = (fn) => {
+    let i = 1;
+    return (...args) => {
+        if(i === 1)
+        {
+            i--;
+            return fn(...args);
+        }
+        else 
+        {
+            return;
+        }
+    }
+};
+
+
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
