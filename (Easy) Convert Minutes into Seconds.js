12084121cd0db65c89a943d465c263547e846fa8
Joseph Lim <limjosephjaine@gmail.com>
2023-06-24 08:59:46 +0800

    Update

---
+++ (Easy) Convert Minutes into Seconds.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes an integer minutes and converts it to seconds.
+
+//-- FOR TESTING --//
+
+convert(5) ➞ 300
+
+convert(3) ➞ 180
+
+convert(2) ➞ 120
+*/
+
+const convert = (a) => a * 60;
+
+console.log(convert(5)) // 300
+console.log(convert(3)) // 180
+console.log(convert(2)) // 120
