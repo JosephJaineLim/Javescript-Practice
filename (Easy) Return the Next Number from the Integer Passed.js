e4d6daefe26ee0e0a87417e1792bc94cd832d80e
Joseph Lim <limjosephjaine@gmail.com>
2023-06-24 09:01:19 +0800

    Update

---
+++ (Easy) Return the Next Number from the Integer Passed.js
@@ -0,0 +1,16 @@
+/*
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+//-- FOR TESTING --//
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+*/
+
+const addition = (a) => ++a;
+
+console.log(addition(0)) // 1
+console.log(addition(9)) // 10
+console.log(addition(-3)) // -2
