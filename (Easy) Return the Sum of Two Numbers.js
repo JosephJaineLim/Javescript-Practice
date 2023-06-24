9a7d151ffe18b68fe1fdac755f1783ff8426f97f
Joseph Lim <limjosephjaine@gmail.com>
2023-06-24 08:58:21 +0800

    Update

---
+++ (Easy) Return the Sum of Two Numbers.js
@@ -0,0 +1,16 @@
+/*
+Create a function that takes two numbers as arguments and returns their sum.
+
+//-- FOR TESTING --//
+addition(3, 2) ➞ 5
+
+addition(-3, -6) ➞ -9
+
+addition(7, 3) ➞ 10
+*/
+
+const addition = (a,b) => a + b;
+
+console.log(addition(3, 2)) // 5
+console.log(addition(-3, -6)) // -9
+console.log(addition(7, 3)) // 10
