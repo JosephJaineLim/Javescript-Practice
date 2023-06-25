0de0c03698c2a79cdf1d852d8016cd10a418710d
Joseph Lim <limjosephjaine@gmail.com>
2023-06-25 19:53:25 +0800

    Update

---
+++ (Easy) Buggy Code (Part 1).js
@@ -0,0 +1,27 @@
+/*
+Fix the code in the code tab to pass this challenge (only syntax errors). 
+Look at the examples below to get an idea of what the function should do.
+
+function cubes(a) {
+	retunr a ** 3
+}
+
+//-- FOR TESTING --//
+cubes(3) ➞ 27
+
+cubes(5) ➞ 125
+
+cubes(10) ➞ 1000
+
+NOTES:
+    READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+
+    Don't overthink this challenge; it's not supposed to be hard.
+*/
+
+const cubes = (a) => a ** 3
+
+
+console.log(cubes(3)) // 27
+console.log(cubes(5)) // 125
+console.log(cubes(10)) // 1000
