536f333a0ec93f42d0f67f8081a36c9d2d7d82aa
Joseph Lim <limjosephjaine@gmail.com>
2023-06-25 22:08:49 +0800

    Update

--- (Easy) Find the nth Tetrahedral Number.js
+++ (Easy) Find the nth Tetrahedral Number.js
@@ -12,10 +12,15 @@ tetra(5) ➞ 35
 
 tetra(6) ➞ 56
 */
+//console.log([5,4,3,2,1].reduceRight((a,b) => c => b))
+
+//console.log(x => 2 * x(2));
+
+
 
 const tetra = (n) => (n * (n + 1) * (n + 2)) / 6
-
 
 console.log(tetra(2)) // 4
 console.log(tetra(5)) // 35
 console.log(tetra(6)) // 56
+
